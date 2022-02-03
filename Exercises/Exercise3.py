14ce921d8755cb9256de27aa40d8ce2f186299be
Henry Tian <tyrneh@gmail.com>
2022-02-03 16:50:06 +0000

    finished q1-3 in ex3

--- Exercises/Exercise3.py
+++ Exercises/Exercise3.py
@@ -11,8 +11,72 @@ def copies_of_string(string,n):
 
 
 
+
 """
 2. Make a two-player Rock-Paper-Scissors game. Ask for players’ names first, then ask them to
 play, make a comparison, print out a message of congratulations to the winner, and finally
 ask if the players want to start a new game.
+"""
+
+def rock_paper_scissors():
+    p1 = input("what is player 1's name? ")
+    p2 = input("what is player 2's name? ")
+
+    loop = True
+    while loop == True:
+        p1_move = input(p1+": what move? (Choose between rock, paper, scissors) ")
+        p2_move = input(p2+": what move? (Choose between rock, paper, scissors) ")
+        if p1_move == p2_move:
+            print("tie. Next round")
+        else:
+            if p1_move == 'rock':
+                if p2_move == 'paper':
+                    print(p2, 'wins')
+                    loop = False
+                else:
+                    print(p1, 'wins')
+                    loop = False
+            if p1_move == 'paper':
+                if p2_move == 'rock':
+                    print(p1, 'wins')
+                    loop = False
+                if p2_move == 'scissors':
+                    print(p2, 'wins')
+                    loop = False
+            if p1_move == 'scissors':
+                if p2_move == 'rock':
+                    print(p2, 'wins')
+                    loop = False
+                if p2_move == 'paper':
+                    print(p1, 'wins')
+                    loop = False
+
+
+
+
+"""
+3. Ask the user for a positive integer and determine whether that number is prime or not.
+"""
+def prime_checker(n):
+    """
+    idea: number is prime iff it's divisible by 1 and itself
+    so iterate from 1 to the number n, and if only 2 remainders are 0, then that number is prime
+
+    this will be O(n) complexity, but I'm sure there's more efficient algorithms by checking GCDs
+    """
+    num_zero_remainders = 0
+
+    for i in range(1,n+1):
+        if n % i == 0:
+            num_zero_remainders += 1
+
+    if num_zero_remainders == 2:
+        print("number is prime")
+    else:
+        print("number is not prime")
+
+
+"""
+4. Implement the bisection method to find the minimum of a univariate function f. 
+Here f' is assumed unknown.
 """
